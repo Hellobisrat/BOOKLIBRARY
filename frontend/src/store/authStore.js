1736a4402050bc67c7fedf1215dc0651f9ca2f39
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2026-08-27 09:55:18 -0500

    adjustment in front

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,6 +1,6 @@
 import {create} from "zustand";
 import axios from 'axios';
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'http://book.local/api';
 axios.defaults.withCredentials = true;
 
 
@@ -35,6 +35,70 @@ export const useAuthStore = create((set)=>({
 
     
       
+    }
+  },
+  
+
+ login : async(username,email,password)=>{
+    set({isLoading:true,message:null,error:null})
+    try {
+      const response = await axios.post(`${API_URL}/login`,{
+        username,
+        password
+      }, { withCredentials: true })
+      const {user, message}=response.data;
+      set({
+        user,
+        isLoading:false,
+        message: response.data.message || "LogIn successful"
+      })
+      return {user,message}
+    } catch (error) {
+      set({
+        error:error.response.data.message || "Error logging in",
+        isLoading:false
+      });
+      throw error;
+      
+    }
+  },
+
+  fetchUser : async()=>{
+    set({fetchingUser:true,error:null})
+    try {
+      const response = await axios.get(`${API_URL}/fetch-user`, { withCredentials: true });
+      set({user: response.data.user,fetchingUser:false})
+    } catch (error) {
+      set({
+         message : error?.response?.data?.message || "Failed to fetch user",
+        error:null,
+        fetchingUser:false,
+        user:null,
+        
+      })
+      throw error;
+
+    }
+  },
+
+  logout: async ()=>{
+    set({isLoading: true, error:null, message: null});
+    try {
+      const response = await axios.post(`${API_URL}/logout`);
+      const {message} = response.data;
+      set({
+        message,
+        isLoading: false,
+        user: null,
+        error:null
+      });
+      return {message}
+    } catch (error) {
+      set({
+        error:error.response.data.message || "Error logging out",
+        isLoading: false,
+      });
+      throw error;
     }
   }
   
